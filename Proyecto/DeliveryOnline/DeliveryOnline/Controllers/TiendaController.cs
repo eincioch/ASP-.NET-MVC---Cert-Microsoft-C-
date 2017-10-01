d0a761ef03936fbc627e9f407ae76e2f227b098b
enrique.incio <enrique.incio@gmail.com>
2017-09-30 22:50:14 -0500

    Avance 01 - Models

---
+++ Proyecto/DeliveryOnline/DeliveryOnline/Controllers/TiendaController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeliveryOnline.Controllers
+{
+    public class TiendaController : Controller
+    {
+        /// <summary>
+        /// Registra una nueva tienda en el sistema
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Guarda el nuevo registro de tienda en persistencia
+        /// </summary>
+        /// <param name="tienda"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Registrar(Models.Tienda tienda)
+        {
+            if (ModelState.IsValid)
+            {
+                tienda.FechaRegsitro = DateTime.Now;
+                // guardarDb(tienda);
+                return RedirectToAction("BuscarTienda", new { id = tienda.CodigoId });
+            }
+            else
+            {
+                return View(tienda);
+            }
+        }
+
+        /// <summary>
+        /// Recupera una tienda
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult BuscarTienda(int id)
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Actualiza los datos de una tienda
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult ActualizarTienda()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ActualizarTienda(Models.Tienda tienda)
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Borra logicamente la tienda del sistema
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult InactivarTienda(int id)
+        {
+            return View();
+        }
+
+        [ActionName("InactivarTienda")]
+        public ActionResult InactivarTiendaConfirmed(int id)
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Retorna toda la lista de tiendas.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ListarTiendas()
+        {
+            return View();
+        }
+
+        public ActionResult ListarTiendasYProductos()
+        {
+            return View();
+        }
+    }
+}
---
+++ Proyecto/DeliveryOnline/DeliveryOnline/Models/DeliveryOnLineContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DeliveryOnline.Models
+{
+    public class DeliveryOnLineContext : DbContext 
+    {
+        public DeliveryOnLineContext(): base("DeliveryOnlineContext")
+        {
+
+        }
+
+        public DbSet<Tienda> Tiendas { get; set; }
+        public DbSet<Persona> Usuarios { get; set; }
+
+        public DbSet<Repartidor> Repartidores { get; set; }
+
+        public DbSet<Producto> Productos { get; set; }
+                
+        public DbSet<TiendaProducto> TiendaProductos { get; set; }
+    }
+}
