6a7e4c4ec4d8d876371af462c6d3bccda6df74d4
enrique.incio <enrique.incio@gmail.com>
2017-10-07 01:59:57 -0500

    Page de Inicio

--- Proyecto/DeliveryOnline/DeliveryOnline/Migrations/Configuration.cs
+++ Proyecto/DeliveryOnline/DeliveryOnline/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace DeliveryOnline.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(DeliveryOnline.Models.DeliveryOnLineContext context)
